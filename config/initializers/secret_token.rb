7fa1af66dba37250ec030182c4b071ed3a1bbfe1
Shreyams Jain <shreyams.jain@sjsu.edu>
2016-07-06 07:28:27 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Udemy::Application.config.secret_key_base = '2e84c8653d56bdef4536024a537aa5b8efa2afbb4dd0484945820f3c8ea7d2bd953ef698b5638586baaa958b819f4795838a194b51df33044606027ef588b089'
