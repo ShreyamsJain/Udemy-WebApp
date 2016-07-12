62c3c517c8975a41994eb27587bd7d42a9c500cd
Shreyams Jain <shreyams.jain@sjsu.edu>
2016-07-12 11:40:17 +0000

    Remove firehose project files

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Udemy
     # -- all .rb files in that directory are automatically loaded.
 
     #added to resolve precompilation issue while deploying to heroku
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
