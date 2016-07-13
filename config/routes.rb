00f76d3a0d21501757df576bddcd416ec19efb2e
Shreyams Jain <shreyams.jain@sjsu.edu>
2016-07-13 11:40:25 +0000

    List all courses in a page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Udemy::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
 
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show, :destroy]
   end
